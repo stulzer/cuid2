df905b31516a8c3101815834f19dae504a98ffba
Rubens Stulzer <rubens.stulzer@gmail.com>
2023-02-18 20:53:42 +0100

    First commit
    Re-implements cuid2 in Ruby.

---
+++ spec/cuid2_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'cuid2'
+
+COLISION_TEST_RUN = ENV.fetch('COLISION_TEST_RUN', 10).to_i
+CUIDS_PER_RUN = ENV.fetch('CUIDS_PER_RUN', 100).to_i
+
+RSpec.describe Cuid2::Generate do
+  it 'has a version number' do
+    expect(Cuid2::VERSION).not_to be nil
+  end
+
+  it 'should not collide' do
+    cuids = []
+
+    COLISION_TEST_RUN.times do
+      CUIDS_PER_RUN.times do
+        cuids << Cuid2::Generate.call
+      end
+    end
+
+    expect(cuids.uniq.size).to eq(cuids.size)
+    expect(cuids.size).to eq(COLISION_TEST_RUN * CUIDS_PER_RUN)
+    expect(cuids.uniq.size).to eq(COLISION_TEST_RUN * CUIDS_PER_RUN)
+    expect(cuids.uniq.sort).to eq(cuids.sort)
+    expect(cuids.sample).to match(/[a-z0-9]{24}/)
+  end
+end
